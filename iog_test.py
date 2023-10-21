392b9c78917fd81bb4fa6405fc356958a79c8225
Josh <joshuaharry@hotmail.co.uk>
2023-10-21 21:34:29 +0100

    Update iog_test.py

--- iog_test.py
+++ iog_test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import requests
 import json
 import re
@@ -9,17 +7,28 @@ url = "https://api.octopus.energy/v1/graphql/"
 APIKeyPattern = re.compile('sk_live_[A-Za-z0-9]{24}?$')
 accountNumberPattern = re.compile('A-[0-9A-F]{8}?$')
 
-def refreshToken(apiKey,accountNumber):
+def validate_input(prompt, pattern):
+    while True:
+        user_input = input(prompt)
+        if pattern.match(user_input):
+            return user_input
+        else:
+            print("Invalid input. Please try again.")
+
+apikey = validate_input("Enter your API key: ", APIKeyPattern)
+accountNumber = validate_input("Enter your account number: ", accountNumberPattern)
+
+def refreshToken(apiKey, accountNumber):
     try:
         query = """
         mutation krakenTokenAuthentication($api: String!) {
-        obtainKrakenToken(input: {APIKey: $api}) {
-            token
-        }
+            obtainKrakenToken(input: {APIKey: $api}) {
+                token
+            }
         }
         """
-        variables = {'api': APIKey}
-        r = requests.post(url, json={'query': query , 'variables': variables})
+        variables = {'api': apiKey}
+        r = requests.post(url, json={'query': query, 'variables': variables})
     except HTTPError as http_err:
         print(f'HTTP Error {http_err}')
     except Exception as err:
@@ -28,108 +37,113 @@ def refreshToken(apiKey,accountNumber):
     jsonResponse = json.loads(r.text)
     return jsonResponse['data']['obtainKrakenToken']['token']
 
+def print_json(json_data):
+    for key, value in json_data.items():
+        print(f"{key}:")
+        if isinstance(value, list):
+            for item in value:
+                print("    -")
+                for sub_key, sub_value in item.items():
+                    print(f"        {sub_key}: {sub_value}")
+        else:
+            for sub_key, sub_value in value.items():
+                print(f"    {sub_key}: {sub_value}")
+        print()
+
 def getObject():
     global authToken
     try:
         query = """
-            query getData($input: String!) {
-                plannedDispatches(accountNumber: $input) {
-                    startDt
-                    endDt
-                    delta
-                }
+        query getData($input: String!) {
+            plannedDispatches(accountNumber: $input) {
+                startDt
+                endDt
+                delta
             }
+        }
         """
         variables = {'input': accountNumber}
-        headers={"Authorization": authToken}
-        r = requests.post(url, json={'query': query , 'variables': variables, 'operationName': 'getData'},headers=headers)
-        return json.loads(r.text)['data']
+        headers = {"Authorization": authToken}
+        r = requests.post(url, json={'query': query, 'variables': variables, 'operationName': 'getData'}, headers=headers)
+        formatted_json = json.loads(r.text)['data']
+        print_json(formatted_json)
+        return formatted_json
     except HTTPError as http_err:
         print(f'HTTP Error {http_err}')
     except Exception as err:
         print(f'Another error occurred: {err}')
+        pass
 
 def getObjectCompleted():
     global authToken
     try:
         query = """
-            query getData($input: String!) {
-                completedDispatches(accountNumber: $input) {
-                    startDt
-                    endDt
-                    delta
-                }
+        query getData($input: String!) {
+            completedDispatches(accountNumber: $input) {
+                startDt
+                endDt
+                delta
             }
+        }
         """
         variables = {'input': accountNumber}
-        headers={"Authorization": authToken}
-        r = requests.post(url, json={'query': query , 'variables': variables, 'operationName': 'getData'},headers=headers)
-        return json.loads(r.text)['data']
+        headers = {"Authorization": authToken}
+        r = requests.post(url, json={'query': query, 'variables': variables, 'operationName': 'getData'}, headers=headers)
+        formatted_json = json.loads(r.text)['data']
+        print_json(formatted_json)
+        return formatted_json
     except HTTPError as http_err:
         print(f'HTTP Error {http_err}')
     except Exception as err:
         print(f'Another error occurred: {err}')
+        pass
 
 def getObjectAccount():
     global authToken
     try:
         query = """
-            query getData($input: String!) {
-                registeredKrakenflexDevice(accountNumber: $input) {
-                    krakenflexDeviceId
-                    provider
-                    vehicleMake
-                    vehicleModel
-                    vehicleBatterySizeInKwh
-                    chargePointMake
-                    chargePointModel
-                    chargePointPowerInKw
-                    status
-                    suspended
-                    hasToken
-                    createdAt
-                }
+        query getData($input: String!) {
+            registeredKrakenflexDevice(accountNumber: $input) {
+                krakenflexDeviceId
+                provider
+                vehicleMake
+                vehicleModel
+                vehicleBatterySizeInKwh
+                chargePointMake
+                chargePointModel
+                chargePointPowerInKw
+                status
+                suspended
+                hasToken
+                createdAt
             }
+        }
         """
         variables = {'input': accountNumber}
-        headers={"Authorization": authToken}
-        r = requests.post(url, json={'query': query , 'variables': variables, 'operationName': 'getData'},headers=headers)
-        return json.loads(r.text)['data']
+        headers = {"Authorization": authToken}
+        r = requests.post(url, json={'query': query, 'variables': variables, 'operationName': 'getData'}, headers=headers)
+        formatted_json = json.loads(r.text)['data']
+        print_json(formatted_json)
+        return formatted_json
     except HTTPError as http_err:
         print(f'HTTP Error {http_err}')
     except Exception as err:
         print(f'Another error occurred: {err}')
+        pass
 
 def getTimesPlanned():
     graphQL = getObject()
-    print(graphQL)
     print()
 
 def getTimesCompleted():
     graphQL = getObjectCompleted()
-    print(graphQL)
     print()
 
 def getKraken():
     graphQL = getObjectAccount()
-    print(graphQL)
     print()
 
-def getInput():
-    global accountNumber
-    accountNumber = input('Enter account number: ')
-    accountMatch = accountNumberPattern.match(accountNumber)
-    if accountMatch is None:
-        raise SystemExit('Account number does not match expected pattern.')
-
-    global APIKey
-    APIKey = input('Enter API key: ')
-    APIMatch = APIKeyPattern.match(APIKey)
-    if APIMatch is None:
-        raise SystemExit('API key does not match expected pattern.')
-
-getInput()
-authToken = refreshToken(accountNumber, APIKey)
+authToken = refreshToken(apikey, accountNumber)
 getKraken()
 getTimesPlanned()
 getTimesCompleted()
